5e4b4e457d66b07318c557e4f4271cf765de3d58
thomasop <tdss33@hotmail.com>
2023-05-26 18:26:55 +0200

    Add: init backend

---
+++ backend/dist/app.js
@@ -0,0 +1,11 @@
+import bodyParser from "body-parser";
+import express from "express";
+import cors from 'cors';
+let app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.get("/", (req, res) => {
+    res.json('test');
+});
+export default app;
---
+++ backend/dist/server.js
@@ -0,0 +1,4 @@
+import app from "./app.js";
+app.listen(8080, () => {
+    console.log('Server listen on 8080');
+});
