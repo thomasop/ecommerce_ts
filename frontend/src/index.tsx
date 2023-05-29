da5cb166de10f803ec84150740710366b5323d66
thomasop <tdss33@hotmail.com>
2023-05-29 20:09:19 +0200

    Add: router and all pages

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Profil from './pages/Profil';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-    <App />
+  <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<Home />} />
+    <Route path='/login' element={<Login />} />
+    <Route path='/progil' element={<Profil />} />
+    <Route path='/*' element={<Home />} />
+  </Routes>
+  </BrowserRouter>
 );
